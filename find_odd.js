22b039aaa896c3530ebee310e011c9e8a6a42bc7
Njeri Kieha <sylvia.kieha@andela.com>
2016-07-25 18:12:50 +0300

    integer appearing odd number of times

---
+++ find_odd.js
@@ -0,0 +1,21 @@
+/*
+	Given an array, find the integer that appears an odd number of times.
+	There will always be only one integer that appears an odd number of times.
+*/
+
+function findOdd(A) {
+	var obj = {};
+	A.map(function(number) {
+		if (obj[number] === undefined) {
+			obj[number] = 1;
+		}
+		else {
+			obj[number] += 1;
+		}
+	});
+	for (var i in obj) {
+		if (obj[i] % 2 !== 0) {
+			return parseInt(i);
+		}
+	}
+}
