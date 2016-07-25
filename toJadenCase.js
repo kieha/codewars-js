8ea41fc0856cc2b0782806b0ebd5761d4d7e823b
Njeri Kieha <sylvia.kieha@andela.com>
2016-07-25 18:12:15 +0300

    capitalizing every word of input string

---
+++ toJadenCase.js
@@ -0,0 +1,11 @@
+// Capitalize every word of the input string
+
+String.prototype.toJadenCase = function () {
+	var newString = '';
+	this.split(' ').forEach(function(word) {
+		var newWord = word[0].toUpperCase() + word.slice(1);
+		newString = newString + newWord + ' ';
+	});
+
+	return newString.trim();
+};
