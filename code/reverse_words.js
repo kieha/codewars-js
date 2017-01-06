5806a35c668f1ed11ce0b0ba243c4e0404ab6d33
Njeri Kieha <sylvia.kieha@andela.com>
2017-01-06 14:49:26 +0300

    Reversing words in a string

---
+++ code/reverse_words.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
+/*
+  Write a reverseWords function that accepts a string a parameter, and reverses
+  each word in the string.Every space should stay, so you cannot use words from Prelude.
+
+  Example:
+  reverseWords("This is an example!"); // returns  "sihT si na !elpmaxe"
+
+*/
+function reverseWords(str) {
+	var stringArray = [];
+
+	str.trim().split(' ').forEach(function (word) {
+		word = word.split('').reverse().join('');
+		stringArray.push(word);
+	});
+
+	return stringArray.join(' ');
+}
+
+console.log(reverseWords('This is an example!'));
